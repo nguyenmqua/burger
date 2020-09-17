e4729d5ac4f34ca4e46e4c267f5113e48613f679
Qua Nguyen <Nguyenmqua@gmail.com>
2020-09-17 17:19:07 -0600

    final product

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Meeses');
+INSERT INTO burgers(burger_name) VALUES ('Bobbi');
+INSERT INTO burgers(burger_name, devoured) VALUES ('d', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('d', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('d', true);
+INSERT INTO burgers(burger_name) VALUES ('Owens');
